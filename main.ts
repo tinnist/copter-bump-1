c66d6f007a2afd1683be51b7b9885f0c8e2d58af
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-15 13:49:26 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,123 @@
+namespace SpriteKind {
+    export const Helicopter = SpriteKind.create()
+    export const cloud = SpriteKind.create()
+    export const LandingPad = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    copter.vy += -1
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    copter.vx += -1
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    copter.vx += 1
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    copter.vy += 1
+})
+sprites.onOverlap(SpriteKind.Helicopter, SpriteKind.cloud, function (sprite, otherSprite) {
+    sprite.x += -1 * sprite.vx
+    sprite.y += -1 * sprite.vy
+    sprite.setVelocity(0, 0)
+    otherSprite.y += -1
+    pause(100)
+    otherSprite.y += 1
+})
+let copter: Sprite = null
+game.splash("Cloud bump", "control pad flying")
+scene.setBackgroundColor(9)
+copter = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 2 2 2 . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . . . 2 . . . . . . . 
+    . . . . . . . . 2 . . . . . . . 
+    . . . 2 2 2 2 2 2 2 2 . . . . . 
+    . 2 2 2 . . . . . . 2 2 2 2 . . 
+    . 2 . . . . . . . . . . . 2 2 . 
+    . 2 . . . . . . . . . . . . 2 . 
+    2 . . . . . . . . . . . . 2 2 . 
+    2 2 . . . . . . . . . 2 2 2 . . 
+    . 2 2 . . . . . 2 2 2 2 . . . . 
+    . . . 2 2 2 2 2 2 . . . . . . . 
+    `, SpriteKind.Helicopter)
+copter.setStayInScreen(true)
+let cloud_1 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 9 9 . . . . . 
+    . . . . 2 2 . . . 2 2 9 . . . . 
+    . . . . 9 . 2 . . . . 2 2 . . . 
+    . . . . 9 . 2 2 . . . . 2 2 . . 
+    . . . . 9 . . 2 . . . . . 2 . . 
+    . . . . 9 9 . 2 2 . . . . 2 . . 
+    . . . . . 9 9 . . 2 2 . 2 2 . . 
+    . . . . . . 9 9 . . 2 . 2 9 . . 
+    . . . . . . . . 9 9 9 2 9 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.cloud)
+let cloud2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 9 9 . . . . . 
+    . . . . 2 2 . . . 2 2 9 . . . . 
+    . . . . 9 . 2 . . . . 2 2 . . . 
+    . . . . 9 . 2 2 . . . . 2 2 . . 
+    . . . . 9 . . 2 . . . . . 2 . . 
+    . . . . 9 9 . 2 2 . . . . 2 . . 
+    . . . . . 9 9 . . 2 2 . 2 2 . . 
+    . . . . . . 9 9 . . 2 . 2 9 . . 
+    . . . . . . . . 9 9 9 2 9 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.cloud)
+let cloud3 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 9 9 . . . . . 
+    . . . . 2 2 . . . 2 2 9 . . . . 
+    . . . . 9 . 2 . . . . 2 2 . . . 
+    . . . . 9 . 2 2 . . . . 2 2 . . 
+    . . . . 9 . . 2 . . . . . 2 . . 
+    . . . . 9 9 . 2 2 . . . . 2 . . 
+    . . . . . 9 9 . . 2 2 . 2 2 . . 
+    . . . . . . 9 9 . . 2 . 2 9 . . 
+    . . . . . . . . 9 9 9 2 9 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.cloud)
+cloud_1.setPosition(20, 30)
+cloud2.setPosition(50, 65)
+cloud3.setPosition(100, 40)
+let landing = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    . . . . . . 3 3 3 3 3 3 3 3 . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.LandingPad)
+landing.y = 125
